91b8ed14b5233f7cce6e3e5b338d3c40672bb129
ethLengfeld <elengfeld@wisc.edu>
2021-01-07 22:03:58 -0600

    added an array

--- C++/Sorting.cpp
+++ C++/Sorting.cpp
@@ -1,9 +1,15 @@
+#include <iostream>
+#include <array>
 
 void quickSort(int[] array, int low, int high) {
 
 }
 
 int main() {
+	int array [5] = { 3, 2, 8, 6, 2};
+
+	quickSort(array, 0, array.size()-1);
+
 
 	return 0;
 }
