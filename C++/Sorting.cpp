32a06b1f9e3231d5ba41e5e7d42c7b3b0b420bd8
ethLengfeld <elengfeld@wisc.edu>
2021-03-03 21:52:23 -0600

    quickSort C++

--- C++/Sorting.cpp
+++ C++/Sorting.cpp
@@ -1,15 +1,49 @@
 #include <iostream>
-#include <array>
 
-void quickSort(int[] array, int low, int high) {
+using namespace std;
 
+void swap(int *x, int *y) {
+	int temp = *x;
+	*x = *y;
+	*y = temp;
 }
 
+int partition(int array[], int low, int high) {
+	int pivot = array[high];
+	int i = low - 1;
+
+	for (int j = low; j < high; j++) {
+		if (array[j] <= pivot) {
+			i++;
+			swap(&array[i], &array[j]);
+		}
+	}
+	swap(&array[i+1], &array[high]);
+	return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+	if (low < high) {
+		int pivot = partition(array, low, high);
+
+		quickSort(array, low, pivot-1);
+
+		quickSort(array, pivot + 1, high);
+	}
+}
+
+void printArray(int array[], int sizeOfArray) {
+	for(int i = 0; i < sizeOfArray; i++) {
+		cout << array[i] << " ";
+	}
+}
 int main() {
 	int array [5] = { 3, 2, 8, 6, 2};
+	int sizeOfArr = sizeof(array);
+	cout << "Size of array is " << sizeOfArr;
+	quickSort(array, 0, sizeof(array)-1);
 
-	quickSort(array, 0, array.size()-1);
-
-
+	printArray(array, sizeOfArr);
+	
 	return 0;
 }
