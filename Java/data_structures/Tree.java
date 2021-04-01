ddf8ce4358950ccfa1d7c9854f5271202e9ee0bc
ethLengfeld <elengfeld@wisc.edu>
2021-03-31 22:06:43 -0500

    tree formatting

--- Java/data_structures/Tree.java
+++ Java/data_structures/Tree.java
@@ -4,7 +4,7 @@ public class Tree implements TreeInterface {
 
 	private TreeNode root = null;
 	private int height = -1;
-	
+
 	public Tree(TreeNode node, int height) {
 		this.root = node;
 		this.height = height;
@@ -19,63 +19,61 @@ public class Tree implements TreeInterface {
 	@Override
 	public void printTreeBoustrophedonOrder(TreeNode node) {
 		int treeHeight = this.getHeight();
-		
-		for(int i = 1; i <= treeHeight; i++) {
-			
-			if(i % 2 == 0) { 
+
+		for (int i = 1; i <= treeHeight; i++) {
+
+			if (i % 2 == 0) {
 				this.getNodeValsAtLevel(this.root, i, true);
 			} else {
 				this.getNodeValsAtLevel(this.root, i, false);
 			}
 		}
 	}
-	
+
 	private void getNodeValsAtLevel(TreeNode root, int level, boolean readRightToLeft) {
-		
+
 		if (root == null) {
 			return;
 		}
-		if (level == 1 ) {
+		if (level == 1) {
 			System.out.print(root.getIntValue() + " ");
-		}
-		else if (level > 1) {
+		} else if (level > 1) {
 			if (readRightToLeft) {
-				this.getNodeValsAtLevel(root.getRightChild(), level-1, readRightToLeft);
-				this.getNodeValsAtLevel(root.getLeftChild(), level-1, readRightToLeft);
-			}
-			else {
-				this.getNodeValsAtLevel(root.getLeftChild(), level-1, readRightToLeft);
-				this.getNodeValsAtLevel(root.getRightChild(), level-1, readRightToLeft);
+				this.getNodeValsAtLevel(root.getRightChild(), level - 1, readRightToLeft);
+				this.getNodeValsAtLevel(root.getLeftChild(), level - 1, readRightToLeft);
+			} else {
+				this.getNodeValsAtLevel(root.getLeftChild(), level - 1, readRightToLeft);
+				this.getNodeValsAtLevel(root.getRightChild(), level - 1, readRightToLeft);
 			}
 		}
 	}
-	
+
 	public TreeNode getRoot() {
 		return this.root;
 	}
-	
+
 	public int getHeight() {
 		return this.height;
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		TreeNode n1 = new TreeNode(1);
-		
+
 		TreeNode n2 = new TreeNode(2);
 		n2.setLeftChild(new TreeNode(4));
 		n2.setRightChild(new TreeNode(5));
-		
+
 		TreeNode n3 = new TreeNode(3);
 		n3.setLeftChild(new TreeNode(6));
 		n3.setRightChild(new TreeNode(7));
-		
+
 		n1.setLeftChild(n2);
 		n1.setRightChild(n3);
-		
+
 		Tree tree = new Tree(n1, 3);
 		tree.printTreeBoustrophedonOrder(tree.getRoot());
-		
+
 	}
 
 }
