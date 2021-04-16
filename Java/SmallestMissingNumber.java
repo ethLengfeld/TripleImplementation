244e7425be516ded71362010da885aa0f6e1a86d
ethLengfeld <elengfeld@wisc.edu>
2021-04-16 18:19:47 -0500

    added new class find smallest missing

---
+++ Java/SmallestMissingNumber.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class SmallestMissingNumber {
+
+	public static int findSmallestNumberCheats(int[] array) {
+
+		Arrays.sort(array);
+		int currNum = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if (currNum > 0 && array[i] != currNum + 1) {
+				return currNum +1;
+			}
+			currNum = array[i];
+		}
+		return 0;
+	}
+
+	@Test
+	public void testSample() {
+		int[] array = { 3, 4, 1, -1 };
+		int smallestMissing = SmallestMissingNumber.findSmallestNumberCheats(array);
+		assertEquals("Two is missing", 2, smallestMissing);
+	}
+}
