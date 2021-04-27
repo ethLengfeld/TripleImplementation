d381677999c2d41606f98cd5677cbec3b5f810b8
ethLengfeld <elengfeld@wisc.edu>
2021-04-27 08:25:32 -0500

    added autocomplete and print array

--- Java/AutocompleteSystem.java
+++ Java/AutocompleteSystem.java
@@ -1,12 +1,38 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 
+import utils.ArrayUtils;
+
 public class AutocompleteSystem {
 
 	public static List<String> getListOfAutoComplete(String startOfWord, String[] words) {
-		
-		return null;
+		char[] wordArr = startOfWord.toCharArray();
+		List<String> autocomplete = new ArrayList<>();
+		// Naive approach
+		boolean diff = false;
+		int i = 0;
+		for(String currWord : words) {
+			char[] currWordArr =  currWord.toCharArray();
+			if(currWord.length() >= startOfWord.length()) {
+				while(i < currWordArr.length && i < wordArr.length) {
+					if(currWordArr[i] != wordArr[i]) {
+						diff = true;
+						break;
+					}
+					i++;
+				}
+				if(!diff) {
+					autocomplete.add(currWord);
+				}
+			}
+			i = 0;
+			diff = false;
+		}
+		return autocomplete;
 	}
 	
 	@Test
@@ -14,5 +40,6 @@ public class AutocompleteSystem {
 		String word = "de";
 		String[] words = {"dog","deer","deal"};
 		List<String> possibleWords = AutocompleteSystem.getListOfAutoComplete(word, words);
+		assertEquals("should return deer and deal", "[deer,deal]", ArrayUtils.printArray(possibleWords));
 	}
 }
--- Java/utils/ArrayUtils.java
+++ Java/utils/ArrayUtils.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.List;
+
 public class ArrayUtils {
 
 	public static void printIntArray(int[] array) {
@@ -16,6 +18,19 @@ public class ArrayUtils {
 		for(int i = 0; i < array.length; i++) {
 			sb.append(array[i]);
 			if(i != array.length-1) {
+				sb.append(",");
+			}
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+
+	public static String printArray(List<String> array) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		for(int i = 0; i < array.size(); i++) {
+			sb.append(array.get(i));
+			if(i != array.size()-1) {
 				sb.append(",");
 			}
 		}
