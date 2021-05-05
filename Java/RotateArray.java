bbca607449fd4d77477ec24b80de820efb50db4d
ethLengfeld <elengfeld@wisc.edu>
2021-05-04 21:47:42 -0500

    saved some space

--- Java/RotateArray.java
+++ Java/RotateArray.java
@@ -13,6 +13,30 @@ public class RotateArray {
 		}
 		return rotated;
 	}
+	
+	public static int[] rotateArraySpace(int[] nums, int k) {
+		int temp = 0;
+		int temp1 = 0;
+		boolean firstPass = true;
+		int pastIndex = 0;
+		int currIndex = 0;
+		for (int i = 0; i < nums.length; i++) {
+			currIndex = (pastIndex+k)%nums.length;
+			
+			if(firstPass) {
+				temp = nums[currIndex];
+				nums[currIndex] = nums[0];
+				firstPass = false;
+			} else {
+				temp1 = nums[currIndex];
+				nums[currIndex] = temp;
+				temp = temp1;
+			}
+			
+			pastIndex = currIndex;
+		}
+		return nums;
+	}
 
 	@Test
 	public void testRotatedArrayNaive() {
@@ -20,11 +44,10 @@ public class RotateArray {
 		int[] rotated = RotateArray.rotateArrayNaive(array,3);
 		assertEquals("rotated naive by 3", "[5,6,7,1,2,3,4]", ArrayUtils.getIntArray(rotated));
 	}
-
-//	@Test
-//	public void testRotatedArraSpace() {
-//		int[] array = {1,2,3,4,5,6,7};
-//		int[] rotated = RotateArray.rotateArraySpace(array,3);
-//		assertEquals("rotated naive by 3", "[5,6,7,1,2,3,4]", ArrayUtils.getIntArray(rotated));
-//	}
+	@Test
+	public void testRotatedArraSpace() {
+		int[] array = {1,2,3,4,5,6,7};
+		int[] rotated = RotateArray.rotateArraySpace(array,3);
+		assertEquals("rotated naive by 3", "[5,6,7,1,2,3,4]", ArrayUtils.getIntArray(rotated));
+	}
 }
