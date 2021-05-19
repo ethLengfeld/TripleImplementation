761b3dfe0984ede0aa52044267ba5cfe6d8ad480
ethLengfeld <elengfeld@wisc.edu>
2021-05-18 22:10:14 -0500

    added running median class

---
+++ Java/RunningMedian.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+public class RunningMedian {
+
+//	public void printRunningMedian(int[] array) {
+//		int high = 0;
+//		int length = 0;
+//		for(int i = 0; i < array.length; i++) {
+//			length = high+1;
+//			if(length%2 == 0 ) {
+//				System.out.print()
+//			} else {
+//				System.out.print()
+//			}
+//			high++;
+//		}
+//	}
+	
+	@Test
+	public void testPrintRunningMedian() {
+		
+	}
+}
