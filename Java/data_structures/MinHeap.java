2c40f702c82d477f31eb408ca56b06259c6e3e42
ethLengfeld <elengfeld@wisc.edu>
2021-05-30 23:09:44 -0500

    finished min heap

--- Java/data_structures/MinHeap.java
+++ Java/data_structures/MinHeap.java
@@ -1,5 +1,7 @@
 package data_structures;
 
+import java.util.Arrays;
+
 public class MinHeap {
 	private int capacity = 10;
 	private int size = 0;
@@ -39,5 +41,59 @@ public class MinHeap {
 		int temp = this.elements[indexOne];
 		this.elements[indexOne] = this.elements[indexTwo];
 		this.elements[indexTwo] = temp;
+	}
+	
+	private void checkCapacity() {
+		if (this.size == this.capacity) {
+			this.elements = Arrays.copyOf(elements, this.capacity*2);
+			this.capacity *= 2;
+		}
+	}
+	
+	public int peek() throws IllegalStateException {
+		if (this.size ==0) throw new IllegalStateException();
+		return this.elements[0];
+	}
+	
+	public int poll() throws IllegalStateException {
+		if (this.size ==0) throw new IllegalStateException();
+		int pollElement = this.elements[0];
+		this.elements[0] = this.elements[this.size-1];
+		this.size--;
+		this.heapifyDown();
+		return pollElement;
+	}
+	
+	public void add(int element) {
+		checkCapacity();
+		this.elements[this.size] = element;
+		this.size++;
+		this.heapifyUp();
+	}
+	
+	private void heapifyDown() {
+		int index = 0;
+		while(this.hasLeftChild(index)) {
+			int smallerChildIndex = this.getLeftChildIndex(index);
+			if(this.hasRightChild(index) && this.rightChild(index) < this.leftChild(index)) {
+				smallerChildIndex = this.getRightChildIndex(index);
+			}
+			
+			if(this.elements[index] < this.elements[smallerChildIndex] ) {
+				break;
+			}
+			else {
+				this.swap(index, smallerChildIndex);
+			}
+		}
+		
+	}
+	
+	private void heapifyUp() {
+		int index = this.size-1;
+		while(this.hasParent(index) && parent(index) > this.elements[index]) {
+			this.swap(index, this.getParentIndex(index));
+			index = this.getParentIndex(index);
+		}
 	}
 }
