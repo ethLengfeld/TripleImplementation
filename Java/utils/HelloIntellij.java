d30d5707e279c77a23f5584ebdec616f1845b83a
Ethan <elengfeld@wisc.edu>
2021-08-22 22:24:12 -0500

    Triple Implementation added scannef code

--- Java/utils/HelloIntellij.java
+++ Java/utils/HelloIntellij.java
@@ -1,9 +1,24 @@
 package utils;
 
+import java.util.Scanner;
+
 public class HelloIntellij {
 
     public static void main(String[] args) {
-        System.out.print("Hello, we're here on IntelliJ!");
-    }
 
+        System.out.println("Hello, we're here on IntelliJ!");
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.print("Pick a number: ");
+        String input = scnr.nextLine();
+        try {
+            int num = Integer.parseInt(input);
+            System.out.println("Number was " + num + " and one more is " + Math.incrementExact(num));
+        } catch (NumberFormatException e) {
+            System.out.println("Error: That was a number" + e);
+        } finally {
+            System.out.print("user input was \"" + input + "\"");
+            scnr.close();
+        }
+    }
 }
