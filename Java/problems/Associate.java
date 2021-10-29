2d08587a0b927ac2dc1d4c229c20dfaaad1c11f3
Ethan <elengfeld@wisc.edu>
2021-10-28 22:40:28 -0500

    added Associate class tbd

---
+++ Java/problems/Associate.java
@@ -0,0 +1,70 @@
+package problems;
+
+public class Associate {
+
+    private static int numAssociates;
+
+    private String firstName;
+    private String lastName;
+    private int birthMonth;
+    private int birthDay;
+    private int birthYear;
+    private long salary;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthMonth() {
+        return this.birthMonth;
+    }
+
+    public void setBirthMonth(int birthMonth) {
+        this.birthMonth = birthMonth;
+    }
+
+    public int getBirthDay() {
+        return this.birthDay;
+    }
+
+    public void setBirthDay(int birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public int getBirthYear() {
+        return this.birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public long getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(long salary) {
+        this.salary = salary;
+    }
+
+    public static int getNumAssociates() {
+        return Associate.numAssociates;
+    }
+
+    public static void setNumAssociates(int numAssociates) {
+        Associate.numAssociates = numAssociates;
+    }
+
+}
