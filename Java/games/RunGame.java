8c40ae90f506039f7e2ccab81a68ae2ed424d0ea
EthanLengfeld.Dev@gmail.com <EthanLengfeld.Dev@gmail.com>
2021-12-08 22:03:45 -0600

    added while statement

--- Java/games/RunGame.java
+++ Java/games/RunGame.java
@@ -15,37 +15,44 @@ public class RunGame {
     }
 
     public static void execute() {
-        System.out.println("Please enter the game you'd like to play:");
-        System.out.println("1. Hangman");
-        System.out.println("Else. EXIT");
-        System.out.print("Enter ->  ");
-        String gameInput = scnr.nextLine();
 
-        switch(gameInput) {
-            case "1":
-                System.out.println("Welcome to Hangman!");
+        String gameInput = "";
+        while(!gameInput.equals("-1")) {
 
-                boolean validSolution = false;
-                String solution = null;
-                while(!validSolution) {
-                    System.out.println("Please enter word/phrase you'd like to guess below");
-                    System.out.print("->   ");
-                    solution = scnr.nextLine().toUpperCase();
-                    validSolution = InputValidationUtil.validateHangmanSolution(solution);
-                    if(!validSolution) {
-                        System.out.println("You entered an invalid solution. Please try again...\n");
+            System.out.println("Please enter the game you'd like to play:");
+            System.out.println("1. Hangman");
+            System.out.println("-1. EXIT");
+            System.out.print("Enter ->  ");
+            gameInput = scnr.nextLine();
+
+            switch (gameInput) {
+                case "1":
+                    System.out.println("Welcome to Hangman!");
+
+                    boolean validSolution = false;
+                    String solution = null;
+                    while (!validSolution) {
+                        System.out.println("Please enter word/phrase you'd like to guess below");
+                        System.out.print("->   ");
+                        solution = scnr.nextLine().toUpperCase();
+                        validSolution = InputValidationUtil.validateHangmanSolution(solution);
+                        if (!validSolution) {
+                            System.out.println("You entered an invalid solution. Please try again...\n");
+                        }
                     }
-                }
-                // TODO need to clear the board to hide solution
-                System.out.println("--------------------------------------------------------------");
-                System.out.println("--------------------     HANGMAN     -------------------------");
-                System.out.println("--------------------------------------------------------------");
-                System.out.println("\n\n\n");
+                    // TODO need to clear the board to hide solution
+                    System.out.println("--------------------------------------------------------------");
+                    System.out.println("--------------------     HANGMAN     -------------------------");
+                    System.out.println("--------------------------------------------------------------");
+                    System.out.println("\n\n\n");
 
-                game = new HangmanGame(solution);
-                game.execute();
-                break;
+                    game = new HangmanGame(solution);
+                    game.execute();
+                    break;
+                case "-1":
+                    System.out.println("Exiting");
+            }
+
         }
-
     }
 }
