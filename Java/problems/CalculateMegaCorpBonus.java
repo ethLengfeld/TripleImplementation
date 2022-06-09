146d16061fe6ff399de5176f1cb3c31b5f884160
EthanLengfeld.Dev@gmail.com <EthanLengfeld.Dev@gmail.com>
2022-06-08 21:56:13 -0500

    bonuses to pay

---
+++ Java/problems/CalculateMegaCorpBonus.java
@@ -0,0 +1,10 @@
+package problems;
+
+public class CalculateMegaCorpBonus {
+
+    public static int[] calculateBonus(int[] linesWritten) {
+        int numWorkers = linesWritten.length;
+        int[] bonuses = new int[numWorkers];
+        return bonuses;
+    }
+}
