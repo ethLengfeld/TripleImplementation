6d503b07abc58e1d186620e555064f94c66d3133
EthanLengfeld.Dev@gmail.com <EthanLengfeld.Dev@gmail.com>
2023-03-07 23:06:16 -0600

    get product array better solutions

--- Java/problems/ProductArrayWithoutI.java
+++ Java/problems/ProductArrayWithoutI.java
@@ -1,6 +1,7 @@
 package problems;
 
 import org.junit.Test;
+import utils.ArrayUtils;
 
 import static org.junit.Assert.assertEquals;
 
@@ -32,6 +33,44 @@ public class ProductArrayWithoutI {
         return productArray;
     }
 
+    public static int[] calculateProductWithoutI_Division(int[] array) {
+        int[] productArray = new int[array.length];
+        int product = array[0];
+        for(int i = 1; i < array.length; i++) {
+            product *= array[i];
+        }
+        for(int j = 0; j < array.length; j++) {
+            productArray[j] = product / array[j];
+        }
+        return productArray;
+    }
+
+    public static int[] calculateProductWithoutI_NoDivision(int[] array) {
+        int[] productArray = new int[array.length];
+        ArrayUtils.printIntArray(array);
+
+
+        int[] prefixArray = new int[array.length];
+        prefixArray[0] = array[0];
+        for (int i = 1; i < array.length; i++) {
+            prefixArray[i] = prefixArray[i-1] * array[i];
+        }
+
+        int[] suffixArray = new int[array.length];
+        suffixArray[array.length-1] = array[array.length-1];
+        for(int j = array.length-2; j >= 0; j--) {
+            suffixArray[j] = suffixArray[j+1] * array[j];
+        }
+
+        productArray[0] = suffixArray[1];
+        productArray[array.length-1] = prefixArray[array.length-2];
+        for(int k = 1; k < array.length-1; k++) {
+            productArray[k] = prefixArray[k-1] * suffixArray[k+1];
+        }
+
+        return productArray;
+    }
+
     @Test
     public void testCalculateProductWithoutI_Brute1() {
         int[] array = { 1, 2, 3, 4, 5 };
@@ -46,6 +85,46 @@ public class ProductArrayWithoutI {
     public void testCalculateProductWithoutI_Brute2() {
         int[] array = { 3, 2, 1 };
         int[] product = ProductArrayWithoutI.calculateProductWithoutI_Brute(array);
+        int[] expected = { 2, 3, 6 };
+        for(int k = 0; k < product.length; k++) {
+            assertEquals(expected[k], product[k]);
+        }
+    }
+
+    @Test
+    public void testCalculateProductWithoutI_Division1() {
+        int[] array = { 1, 2, 3, 4, 5 };
+        int[] product = ProductArrayWithoutI.calculateProductWithoutI_Division(array);
+        int[] expected = { 120, 60, 40, 30, 24 };
+        for(int k = 0; k < product.length; k++) {
+            assertEquals(expected[k], product[k]);
+        }
+    }
+
+    @Test
+    public void testCalculateProductWithoutI_Division2() {
+        int[] array = { 3, 2, 1 };
+        int[] product = ProductArrayWithoutI.calculateProductWithoutI_Division(array);
+        int[] expected = { 2, 3, 6 };
+        for(int k = 0; k < product.length; k++) {
+            assertEquals(expected[k], product[k]);
+        }
+    }
+
+    @Test
+    public void testCalculateProductWithoutI_NoDivision1() {
+        int[] array = { 1, 2, 3, 4, 5 };
+        int[] product = ProductArrayWithoutI.calculateProductWithoutI_NoDivision(array);
+        int[] expected = { 120, 60, 40, 30, 24 };
+        for(int k = 0; k < product.length; k++) {
+            assertEquals(expected[k], product[k]);
+        }
+    }
+
+    @Test
+    public void testCalculateProductWithoutI_NoDivision2() {
+        int[] array = { 3, 2, 1 };
+        int[] product = ProductArrayWithoutI.calculateProductWithoutI_NoDivision(array);
         int[] expected = { 2, 3, 6 };
         for(int k = 0; k < product.length; k++) {
             assertEquals(expected[k], product[k]);
